13eb0213c9d03523a2584f00d753173b1c56385b
SvetaKul <Svetlana.Kuleshova@russianpost.ru>
2019-02-27 14:22:34 +0300

    Второй файл

---
+++ src/main/java/Zadanie2.java
@@ -0,0 +1,17 @@
+public class Zadanie2 {
+
+    public static void main(String args[]){
+
+        int a = 19;
+        int b = 22;
+
+        if (a > b){
+            System.out.println(" " + b);
+        }
+        else {
+            System.out.println("Ближайшее число к 20 = " + a);
+        }
+
+    }
+
+}
