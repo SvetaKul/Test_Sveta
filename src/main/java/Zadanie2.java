c200255f2f3a368ea9d9c76f7af5c4838c9497b8
SvetaKul <Svetlana.Kuleshova@russianpost.ru>
2019-03-01 14:13:41 +0300

    Второй файл

--- src/main/java/Zadanie2.java
+++ src/main/java/Zadanie2.java
@@ -1,29 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Zadanie2 {
 
     public static void main(String args[]) {
 
-        int a = 10;
-        int b = 30;
-        int c = 20 - a;
-        int d = 20 - b;
+        Scanner input = new Scanner(System.in);
 
 
-        if (Math.abs (c) == Math.abs(d)) {
-            System.out.println("Равноудалены");
+        System.out.println("введите число");
+
+        int a = input.nextInt();
+
+        if (a%2==0)
+        {System.out.println("Число " + a + " четное"); }
+        else {System.out.println("Число " + a + " нечетное");}
+
+
+        System.out.println("введите второе число");
+        int b = input.nextInt();
+
+        if (b%2==0)
+        {System.out.println("Число " + b + " четное"); }
+        else {System.out.println("Число " + b + " нечетное"); }
+
+
+        if (Math.abs (20-a) == Math.abs(20-b)) {
+            System.out.println("Введенные числа равноудалены");
         }
-        else if (Math.abs (c) > Math.abs(d)) {
+        else if (Math.abs (20-a) > Math.abs(20-b)) {
             System.out.println("Ближайшее число к 20 = " + b);
         }
         else  {
             System.out.println("Ближайшее число к 20 = " + a);
         }
 
+        Random rand = new Random();
+        int s = rand.nextInt(151)+5;
+        if ((25 <= s)&&(s <= 100)) System.out.printf( "Рандомное число отрезка [5;155] "+ s +" попало в отрезок чисел от 25 до 100");
+        else System.out.printf("Рандомное число отрезка [5;155] "+ s +" не попало в отрезок чисел от 25 до 100)");
 
-        int n = 9;
 
-        if (n%2==0)
-        {System.out.println("Четное"); }
-        else {System.out.println("Нечетное");
-    }
+
+
+
+
+
 }
 }
+
