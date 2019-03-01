5c18eb279d4c8d01faa30f9a3f1791689f1fd916
SvetaKul <Svetlana.Kuleshova@russianpost.ru>
2019-03-01 14:20:44 +0300

    Второй файл

--- src/main/java/Zadanie2.java
+++ src/main/java/Zadanie2.java
@@ -12,40 +12,38 @@ public class Zadanie2 {
 
         int a = input.nextInt();
 
-        if (a%2==0)
-        {System.out.println("Число " + a + " четное"); }
-        else {System.out.println("Число " + a + " нечетное");}
+        if (a % 2 == 0) {
+            System.out.println("Число " + a + " четное");
+        } else {
+            System.out.println("Число " + a + " нечетное");
+        }
 
 
         System.out.println("введите второе число");
         int b = input.nextInt();
 
-        if (b%2==0)
-        {System.out.println("Число " + b + " четное"); }
-        else {System.out.println("Число " + b + " нечетное"); }
+        if (b % 2 == 0) {
+            System.out.println("Число " + b + " четное");
+        } else {
+            System.out.println("Число " + b + " нечетное");
+        }
 
 
-        if (Math.abs (20-a) == Math.abs(20-b)) {
+        if (Math.abs(20 - a) == Math.abs(20 - b)) {
             System.out.println("Введенные числа равноудалены");
-        }
-        else if (Math.abs (20-a) > Math.abs(20-b)) {
+        } else if (Math.abs(20 - a) > Math.abs(20 - b)) {
             System.out.println("Ближайшее число к 20 = " + b);
-        }
-        else  {
+        } else {
             System.out.println("Ближайшее число к 20 = " + a);
         }
 
         Random rand = new Random();
-        int s = rand.nextInt(151)+5;
-        if ((25 <= s)&&(s <= 100)) System.out.printf( "Рандомное число отрезка [5;155] "+ s +" попало в отрезок чисел от 25 до 100");
-        else System.out.printf("Рандомное число отрезка [5;155] "+ s +" не попало в отрезок чисел от 25 до 100)");
+        int s = rand.nextInt(151) + 5;
+        if ((25 <= s) && (s <= 100))
+            System.out.printf("Рандомное число отрезка [5;155] " + s + " попало в отрезок чисел от 25 до 100");
+        else System.out.printf("Рандомное число отрезка [5;155] " + s + " не попало в отрезок чисел от 25 до 100)");
 
 
-
-
-
-
-
-}
+    }
 }
 
